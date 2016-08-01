a38139bdac921cc29f7c7e2f96c742a1f16780c3
Milind L <milindl@iitk.ac.in>
2016-08-01 19:21:47 +0530

    Added basic plotting abilities

--- graph.js
+++ graph.js
@@ -15,7 +15,18 @@ Graph.prototype.normalizedData = function() {
   return normalizedDataset;
 }
 
+Graph.prototype.plotGraph = function(cvas, width, height) {
+  cvas.width = width;
+  cvas.height = height;
+  var ctx = cvas.getContext("2d");
+  var normalizedDataset = this.normalizedData();
+  var perBarWidth = Math.floor(width/normalizedDataset.length);
+  for(var i=0; i!=normalizedDataset.length; i++) {
+    ctx.fillRect(i*perBarWidth, (height-(height*normalizedDataset[i])), perBarWidth, height*normalizedDataset[i]);
+  }
+}
+
 function test(){
   var g = new Graph([60, 10, 20, 5, 5], null);
-  alert(g.normalizedDataset());
+  g.plotGraph(document.getElementById("t"), 500,500);
 }
