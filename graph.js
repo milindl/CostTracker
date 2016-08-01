29852377387ec80fa8313590a94c7a0a2a3aa517
Milind L <milindl@iitk.ac.in>
2016-08-01 22:28:01 +0530

    Made labels and values display

--- graph.js
+++ graph.js
@@ -19,18 +19,25 @@ Graph.prototype.plotGraph = function(cvas, width, height) {
   cvas.width = width;
   cvas.height = height;
   var fontConstant = 50;
-  height=height-fontConstant; //For text and stuff ABOVE the actual height
+  var labelConstant = 50;
+  height=height-fontConstant-labelConstant; //For text and stuff ABOVE the actual height
   var ctx = cvas.getContext("2d");
   var normalizedDataset = this.normalizedData();
   var perBarWidth = Math.floor(width/normalizedDataset.length);
   for(var i=0; i!=normalizedDataset.length; i++) {
+    var color=Math.floor(360*i/normalizedDataset.length);
+    color = "hsl(" + color + ",60%, 50%)";
+    ctx.fillStyle=color;
     ctx.fillRect(i*perBarWidth, (fontConstant+height-(height*normalizedDataset[i])), perBarWidth, height*normalizedDataset[i]);
     ctx.textAlign = "center";
     ctx.fillText(""+(this.dataset[i]), Math.floor((i+0.5) * perBarWidth), (fontConstant-10+height-(height*normalizedDataset[i])));
+    ctx.fillText(""+(this.fields[i]), Math.floor((i+0.5) * perBarWidth), (fontConstant+labelConstant+(height)));
   }
 }
 
 function test(){
-  var g = new Graph([60, 10, 20, 5, 10, 20, 60, 70, 80], null);
-  g.plotGraph(document.getElementById("t"), 500,500);
+  var d = [10, 19, 20, 2, 3, 8.0, 9.7, 8.34];
+  var f = ['timeless', 'tradefree', 'teign', 'lessthat', 'ticktock', 'reimann', 'feynman', 'lolilike'];
+  var g = new Graph(d,f);
+  g.plotGraph(document.getElementById("t"), window.innerWidth,400);
 }
