5a2996a383db9987b058365c4ad0729c2a6f059b
Milind L <milindl@iitk.ac.in>
2016-08-03 14:08:57 +0530

    Made file loading util

--- node/backbone.js
+++ node/backbone.js
@@ -1,6 +1,7 @@
 var expenses = require("../expense.js");
 var fs = require("fs");
 var fPath = "data.txt";
+var loadedFile = false;
 /*
   Super interesting thing:
 */
@@ -13,7 +14,22 @@ exports.add = function(exp) {
     console.log(fPath);
     console.log(saveable);
     console.log("Appended");
+    if(loadedFile) loadedFile+=saveable;
   });
 };
-
+exports.load = function(cback) {
+  if(loadedFile) cback(loadedFile);
+  fs.readFile(fPath, function(err, d) {
+    if(err) return err;
+    loadedFile=d.toString();
+    cback(loadedFile);
+  });
+};
+// exports.load(function(t) {
+//   console.log(t);
+// })
+// // exports
 // exports.add(new expenses.Expense(1, "a", "Cat", new Date()));
+// exports.load(function(t) {
+//   console.log(t);
+// })
