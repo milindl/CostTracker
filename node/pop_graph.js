93935ccdd39dac06b81bfc232659ba4984962b34
Milind L <milindl@iitk.ac.in>
2016-08-03 11:31:23 +0530

    Can handle add-requests now, using backbone.js

--- node/pop_graph.js
+++ node/pop_graph.js
@@ -5,5 +5,13 @@ var expenses = require("../expense.js");
 var backbone = require("./backbone.js");
 var app = express();
 
+app.get("/add", function(req,res) {
+  //Need to fetch parameters from request
+  var query = url.parse(req.url, true).query;
+  console.log(query);
+  var exp = new expenses.Expense(parseInt(query['amount']), query['remarks'], query['category'], new Date());
+  backbone.add(exp);
+  res.redirect("/");
+});
 app.use("/", express.static("../")) //Serve all the static files
 app.listen(7080);
